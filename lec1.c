a3a5dd0d79772d73739b6dba3af073cea7f6959d
Isha536 <chauhanisha134@gmail.com>
2024-07-18 18:48:52 +0530

    Add files via upload

---
+++ lec1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	printf("BIO-DATA \n");
+	printf("Name: Chauhan Isha \n");
+	printf("AGE:18 \n");
+	printf("School: Gyanmanjari vidyapeeth");
+	
+	
+}
