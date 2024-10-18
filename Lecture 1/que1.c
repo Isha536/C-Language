245fbfb98c092a7de1927bbc089ebae481dcbe95
Isha536 <chauhanisha134@gmail.com>
2024-10-18 18:31:24 +0530

    Add files via upload

---
+++ Lecture 1/que1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	//wap to program to print name age and school.
+	
+	printf("My name is Isha Chauhan.\n");
+	printf("My age is 18.\n");
+	printf("My school was Gyanmanjari Vidyapeeth.");
+}
