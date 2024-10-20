601a9d97e559ce864369fefc66af1bdc76617ca7
Isha536 <chauhanisha134@gmail.com>
2024-10-20 14:30:38 +0530

    Add files via upload

---
+++ Lecture3/que1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	//write a program to perform the arithmetic operator -,*,/,+
+	int a=10,b=20;
+	
+	//for +
+	printf("%d + %d = %d\n",a,b,a+b);
+	//for -
+	printf("%d - %d = %d\n",a,b,a-b);
+	//for *
+	printf("%d * %d = %d\n",a,b,a*b);
+	//for /
+	printf("%d / %d = %d\n",a,b,a/b);
+}
