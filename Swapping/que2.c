4015a10b3155a8828689e65bd60f14c2c44a3052
Isha536 <chauhanisha134@gmail.com>
2024-10-20 14:20:58 +0530

    Add files via upload

---
+++ Swapping/que2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int a,b;
+	
+	printf("ENTER THE VALUE OF a:");
+	scanf("%d",&a);
+	
+	printf("ENTER THE VALUE OF b:");
+	scanf("%d",&b);
+	
+	a=a+b;
+	b=a-b;
+	a=a-b;
+	
+	printf("the ans is:%d\n",a);
+	printf("the ans is:%d",b);
+}
